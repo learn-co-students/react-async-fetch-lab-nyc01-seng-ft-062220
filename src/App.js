078ac9dc4e2a295ea117814bef92d93768de1008
dukhyunko93 <dukhyunko93@gmail.com>
2020-08-28 08:39:21 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React from 'react';
+
+class App extends React.Component {
+
+    state = {
+        people: []
+    }
+
+    list = () => this.state.people.map(person => <li>{person.name}{person.craft}</li>)
+
+    render(){
+        return(
+            <React.Fragment>
+                <h1>hellos</h1>
+                <ul>
+                    {this.list()}
+                </ul>
+            </React.Fragment>
+        )
+    }
+
+    async componentDidMount(){
+        let response = await fetch("http://api.open-notify.org/astros.json");
+        let data = await response.json();
+        this.setState({people: data.people});
+    }
+
+}
+
+export default App;
