0901113bf11d7162a78dbbc73abcdd9fefd2ba63
yulia <juliana.ny2008@gmail.com>
2020-08-28 08:24:30 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
   componentDidMount() {
        fetch('http://api.open-notify.org/astros.json')
       .then(resp => resp.json())
-//.then(resp => console.log(resp))
       .then(resp =>  this.setState({ people: resp.people }))
 
   }
